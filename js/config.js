ca60b4875c28618e538c5964d1306778b93a32f7
davidmonov <72251276+davidmonov@users.noreply.github.com>
2025-02-06 15:02:15 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-central-1' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://kyv1es0g88.execute-api.eu-central-1.amazonaws.com/Dev' 
     }
 };
